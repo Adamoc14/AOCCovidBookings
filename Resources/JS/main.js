57286555133944748326e82df6dc01f9e91ce0c2
adamoc <oceallaighadam96@gmail.com>
2020-09-13 22:59:49 +0100

    Got the update route working , just need to style it now

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -21,7 +21,10 @@ const displayUserView = async() => {
     userDetails.Appointments.map(appt =>  
         `<div class="appointment_container">
             <div class="first_container">
-                <div class="date_square">${appt.DayDate}</div>
+                <div class="date_square">
+                    <h5>${appt.Month}</h5>
+                    ${appt.DayDate}
+                </div>
                 <div class="user_details_container">
                     <div class="name_container">
                         <h2>Name: ${userDetails.firstName} ${userDetails.Surname}</h2>
