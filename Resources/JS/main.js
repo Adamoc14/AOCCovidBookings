75c57b36e4d704e32f858e12f87bfd504106abae
adamoc <oceallaighadam96@gmail.com>
2020-10-06 19:26:53 +0100

    Added the email functionality

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -200,6 +200,7 @@ const createAppointmentBtnClick = () => {
             return
         }
         appointment_Details["firstName"] = formData.get('firstName')
+        appointment_Details["Email"] = formData.get('Email')
         appointment_Details["Surname"] = formData.get('Surname')
         appointment_Details["Mobile"] = formData.get('Mobile')
         appointment_Details["DOB"] = formData.get('DOB')
