deb2c5fd776fc32713b7078c398e5bdb1ea3f018
adamoc <oceallaighadam96@gmail.com>
2021-01-21 12:51:33 +0000

    Changing up the form to include new parts

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,6 +1,6 @@
 // Global Variable Declarations and Function Definitions
 const appointment_Details = {},
-    url = "https://whmc-server.herokuapp.com/";
+    url = "http://localhost:8000/";
 let appointments_Saved = [],
     appointments_Data = [],
     clinic_Data = [],
